ca40569e47aee321f0adaeaaede5c3b06d42f812
rakesh guntuka <rakeshguntuka75@gmail.com>
2021-01-25 12:51:36 +0530

    intial commit

---
+++ src/main/java/com/example/demo/JrtpSbApp4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrtpSbApp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrtpSbApp4Application.class, args);
+	}
+
+}
