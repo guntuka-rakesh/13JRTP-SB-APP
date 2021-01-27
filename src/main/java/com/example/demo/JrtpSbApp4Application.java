b6c3169878e46e445e1a5d0f706d2313e1c08c3b
rakesh guntuka <rakeshguntuka75@gmail.com>
2021-01-27 12:36:12 +0530

    feature-301 got chanched

--- src/main/java/com/example/demo/JrtpSbApp4Application.java
+++ src/main/java/com/example/demo/JrtpSbApp4Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JrtpSbApp4Application {
+          Map<String,Integer> m= new HashMap();
 
 	public static void main(String[] args) {
 		SpringApplication.run(JrtpSbApp4Application.class, args);
@@ -32,5 +33,8 @@ public class JrtpSbApp4Application {
 		System.out.println(i);
 		System.out.println(g);
 	}
+        public void amazon(){
+         //logic
+        }
 
 }
