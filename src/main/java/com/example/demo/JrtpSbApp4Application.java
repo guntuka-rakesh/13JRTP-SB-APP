e0dda8110467df8055146bf836fa379190c97691
hp <rakeshguntuka75@gmail.com>
2021-02-18 15:40:04 +0530

    variables are added to main class

--- src/main/java/com/example/demo/JrtpSbApp4Application.java
+++ src/main/java/com/example/demo/JrtpSbApp4Application.java
@@ -8,29 +8,10 @@ public class JrtpSbApp4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JrtpSbApp4Application.class, args);
-		System.out.println("hii");
-		System.out.println("JRTP-13");
-
-		System.out.println("HIS-134");
-		System.out.println("fds");
-		//////////////////////////////////////
-                int d=10;
-                int e30;
-                int f=40;
-                //////////////////////////////
-
-		int i=10;
-		String g="hello";
- 
-		System.out.println(i);
-		System.out.println(g);
-		
-		//////////////////////////////////////
-		int j=10;
-		String d="hello";
-		
-		System.out.println(i);
-		System.out.println(g);
+		int a=10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
